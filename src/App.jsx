4bd26adef10fa31c31e47044b3b708449f1ba5f8
piyush <piyushurodebgt@gmail.com>
2024-11-19 08:11:08 +0530

    asaasdasdsads

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+
 import Homepage from "./Homepage/Homepage.jsx"
 
 const App = () => {
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import logo from "../assets/images/doglogo.png";
 
-constNavbar = () => {
+const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isNavVisible, setIsNavVisible] = useState(false);
 
@@ -28,91 +27,96 @@ constNavbar = () => {
   };
 
   return (
-    <>
-      <nav className={`w-full overflow-x-hidden  top-0 border-b border-none dark:border-gray-600`}>
-        <div className="relative max-w-full flex flex-wrap items-center justify-between mx-auto p-4 px-4 md:p-4 md:px-8">
-          <Link to="/" onClick={closeMenu}>
-            <div className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img loading="lazy" src={logo} className="w-[9rem] phone:w-[5.5rem] h-auto" alt="smartBCpresale" />
-            </div>
-          </Link>
+    <nav className={`w-full overflow-x-hidden top-0 border-b border-none dark:border-gray-600`}>
+      <div className="relative max-w-full flex flex-wrap items-center justify-between mx-auto p-4 px-4 md:p-4 md:px-8">
+        {/* Replace Link with an anchor tag */}
+        <a href="#home" onClick={closeMenu}>
+          <div className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img
+              loading="lazy"
+              src={logo}
+              className="w-[9rem] phone:w-[5.5rem] h-auto"
+              alt="doogscoin"
+            />
+          </div>
+        </a>
 
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <div className='flex gap-3 phone:gap-1 items-center'>
-              {/* <a target="blank" href="https://smartbcconcept.net/MemberPanel/Login.aspx" className="rbtn h-fit rounded-full">
-                <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-2 phone:text-sm">Sign In</span>
-              </a> */}
-              <a target="blank" href="/" className="rbtn h-fit rounded-full">
-                <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-2 phone:text-sm text-orange-500 rounded-xl border-2 border-orange-500">BUY DOOGS</span>
-              </a>
-            </div>
-
-            <button
-              onClick={toggleNav}
-              type="button"
-              className="inline-flex items-center p-1 w-9 h-9 justify-center text-sm text-red-100 bg-gray-900 border border-[--primary-color] rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-[--prime-color] hover:bg-gray-900"
-              aria-controls="navbar-sticky1"
-              aria-expanded={isNavVisible}
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className='flex gap-3 phone:gap-1 items-center'>
+            <a
+              target="blank"
+              href="/"
+              className="rbtn h-fit rounded-full"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-4 h-5"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
+              <span className="text rounded-full px-5 py-2 phone:py-1 phone:px-2 phone:text-sm text-orange-500 rounded-xl border-2 border-orange-500">
+                BUY DOOGS
+              </span>
+            </a>
           </div>
 
-          <div
-            id="navbar-sticky1"
-            className={`items-center justify-between w-[99%] md:flex md:w-auto md:order-1 ${isNavVisible ? 'block' : 'hidden'}`}
+          <button
+            onClick={toggleNav}
+            type="button"
+            className="inline-flex items-center p-1 w-9 h-9 justify-center text-sm text-red-100 bg-gray-900 border border-[--primary-color] rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-[--prime-color] hover:bg-gray-900"
+            aria-controls="navbar-sticky1"
+            aria-expanded={isNavVisible}
           >
-            <ul className="navcar flex flex-col p-4 md:py-1.5 mt-4 font-medium border border-[--primary-color] md:rounded-[50px] rounded-lg md:space-x-[3vmax] md:flex-row md:mt-0 md:border-0 text-black tablet:bg-gray-900">
-            <li className="md:border-none border-b-[1px] border-gradient">
-    <a href="#home" onClick={closeMenu}>
-      <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">Home</div>
-    </a>
-  </li>
-  <li className="md:border-none border-b-[1px] border-gradient">
-    <a href="#about" onClick={closeMenu}>
-      <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">About</div>
-    </a>
-  </li>
-  <li className="md:border-none border-b-[1px] border-gradient">
-    <a href="#token" onClick={closeMenu}>
-      <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">Tokenomics</div>
-    </a>
-  </li>
-  {/* <li className="md:border-none border-b-[1px] border-gradient">
-    <a href="#roadmap" onClick={closeMenu}>
-      <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">Roadmap</div>
-    </a>
-  </li> */}
-  <li className="md:border-none border-b-[1px] border-gradient">
-    <a href="#howtobuy" onClick={closeMenu}>
-      <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">How to Buy</div>
-    </a>
-  </li>
-              {/* <li className="md:border-none border-b-[1px] border-gradient">
-                <Link to="/Privacypolicy" onClick={closeMenu}>
-                  <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">Privacy Policy</div>
-                </Link>
-              </li> */}
-            </ul>
-          </div>
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-4 h-5"
+              aria-hidden="true"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
         </div>
-      </nav>
-    </>
+
+        <div
+          id="navbar-sticky1"
+          className={`items-center justify-between w-[99%] md:flex md:w-auto md:order-1 ${isNavVisible ? 'block' : 'hidden'}`}
+        >
+          <ul className="navcar flex flex-col p-4 md:py-1.5 mt-4 font-medium border border-[--primary-color] md:rounded-[50px] rounded-lg md:space-x-[3vmax] md:flex-row md:mt-0 md:border-0 text-black tablet:bg-gray-900">
+            <li className="md:border-none border-b-[1px] border-gradient">
+              <a href="#home" onClick={closeMenu}>
+                <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">
+                  Home
+                </div>
+              </a>
+            </li>
+            <li className="md:border-none border-b-[1px] border-gradient">
+              <a href="#about" onClick={closeMenu}>
+                <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">
+                  About
+                </div>
+              </a>
+            </li>
+            <li className="md:border-none border-b-[1px] border-gradient">
+              <a href="#token" onClick={closeMenu}>
+                <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">
+                  Tokenomics
+                </div>
+              </a>
+            </li>
+            <li className="md:border-none border-b-[1px] border-gradient">
+              <a href="#howtobuy" onClick={closeMenu}>
+                <div className="font-normal block py-2 px-3 text-black rounded hover:bg-gray-700 md:hover:bg-transparent md:p-0 hover:text-orange-600 font-outfit">
+                  How to Buy
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
